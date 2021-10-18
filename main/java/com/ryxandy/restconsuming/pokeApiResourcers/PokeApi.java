2d5361af1bf74d6fc36900ba067c06f96de58e5b
Alexandre Lopes <33386705+ryxandy@users.noreply.github.com>
2021-10-18 15:39:13 -0300

    added some endpoints
    search for fraqueza and all and only one pokemon

--- main/java/com/ryxandy/restconsuming/pokeApiResourcers/PokeApi.java
+++ main/java/com/ryxandy/restconsuming/pokeApiResourcers/PokeApi.java
@@ -4,14 +4,19 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @FeignClient(url = "https://pokeapi-br.herokuapp.com/", name="PokeApi")
 public interface PokeApi {
 
-    @GetMapping("pokemons")
-    Pokemon buscarTodosOsPokemons();
+    @GetMapping("/pokemons")
+    List<Pokemon> buscarTodosPokemons();
 
     @GetMapping(path = "/pokemon/{id}")
     Pokemon buscarPorID(@PathVariable("id") String id);
+
+    @GetMapping(path = "/pokemon/fraqueza/{fraqueza}")
+    Pokemon buscarPorFraqueza(@PathVariable String fraqueza);
 
 
 
--- main/java/com/ryxandy/restconsuming/pokeApiResourcers/PokeApiService.java
+++ main/java/com/ryxandy/restconsuming/pokeApiResourcers/PokeApiService.java
@@ -1,12 +1,11 @@
 package com.ryxandy.restconsuming.pokeApiResourcers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
+import java.util.List;
 
 @RestController
 public class PokeApiService {
@@ -14,11 +13,20 @@ public class PokeApiService {
     @Autowired
     private PokeApi pokeApi;
 
-    @GetMapping(path = "/pokemons/{id}")
-    public ResponseEntity<Pokemon> getTodosPokemons(@PathVariable String id){
+    @GetMapping(path = "/pokemon/{id}")
+    public Pokemon buscarPorID(@PathVariable String id){
         System.out.println("Procuramos pelo Pokemon em específico");
-        Pokemon pokemon = pokeApi.buscarPorID(id);
-        System.out.println(Objects.requireNonNull(ResponseEntity.ok().body(pokemon).getBody()).toString());
-        return pokemon != null? ResponseEntity.ok().body(pokemon): ResponseEntity.notFound().build();
+        return pokeApi.buscarPorID(id);
+    }
+
+    @GetMapping(path = "/pokemons")
+    public List<Pokemon>BuscarTodosPokemons(){
+        return pokeApi.buscarTodosPokemons();
+    }
+
+    @GetMapping(path = "/pokemon/fraqueza/{fraqueza}")
+    public Pokemon buscarPorFraqueza(@PathVariable String fraqueza){
+        System.out.println("Procurando por pokemon com essa fraqueza");
+        return pokeApi.buscarPorFraqueza(fraqueza);
     }
 }
